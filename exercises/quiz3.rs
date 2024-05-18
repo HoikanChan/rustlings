a2e089bc3a89b7803fd3a158200a31a5164d2ef8
Hoikan Chan <hoikan.chen@gmail.com>
2024-05-18 09:12:52 +0000

    FINISH: iter

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,8 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
@@ -26,8 +24,23 @@ pub struct ReportCard {
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
+    }
+
+    pub fn printAlphabetically(&self) -> String {
+        let mut alphabeticalGrade: &str = "B";
+        if self.grade < 2.5 {
+            alphabeticalGrade = "A+"
+        }
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name,
+            &self.student_age,
+            alphabeticalGrade,
+        )
     }
 }
 
@@ -57,7 +70,7 @@ mod tests {
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.printAlphabetically(),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
